968b0d61dc3ca9f8b9db2752a9cd70075e5272e8
Huseyin <hakahakozdemir35@gmail.com>
2024-09-01 19:30:45 +0100

    Day 39 Collections

--- src/Muhtar_SDET/Day39_Collections/SetDemo.java
+++ src/Muhtar_SDET/Day39_Collections/SetDemo.java
@@ -23,5 +23,33 @@ public class SetDemo {
 
         System.out.println("-----------------------------------");
 
+        String[] words = {"Java", "Java", "Java", "Python", "Python", "C#", "C++", "Ruby","C#", "C#"};
+        Set<String> result = new LinkedHashSet<>();
+        result.addAll(Arrays.asList(words));
+        System.out.println(result);
+
+        /*
+        for (String each : result) {
+            System.out.println(each);
+
+        }*/
+
+        System.out.println(new ArrayList<>(result).get(1)); // result is collections type
+
+        words = result.toArray(new String[0]);
+
+        System.out.println("Words Array: " + Arrays.toString(words));
+        // Any of collections type you can convert to array with toArray(); because array is faster
+
+
+
+        System.out.println("-----------------------------------");
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(10,20,12,3,6,56,96,36,748,85,2,3,6,78,7,1,1,1,1,2,2,2));
+
+        Set<Integer> n = new TreeSet<>(numbers); // parameters must be collections type, List is collections type,
+        System.out.println(n);
+
     }
 }
