de016b855801a0d62316ed401e046e05102b7599
Huseyin <78041148+HsynHsyn@users.noreply.github.com>
2024-09-21 17:39:12 +0100

    Create D.java

---
+++ src/Saturday_Reviews/week18_git_review/D.java
@@ -0,0 +1,8 @@
+package Saturday_Reviews.week18_git_review;
+
+public class D {
+    public static void main(String[] args) {
+        System.out.println("From another person");
+      // master branch
+    }
+}
