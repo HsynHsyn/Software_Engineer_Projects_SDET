e34a6af994cdc051a8f30eaf49d8fed09ffc7c2c
HsynHsyn <hakanhakanozdemir35@gmail.com>
2024-10-08 17:44:42 +0100

    IQ2

--- src/java_Coding_Interview_Questions/Q2_ReverseNegativeNumber.java
+++ src/java_Coding_Interview_Questions/Q2_ReverseNegativeNumber.java
@@ -3,22 +3,35 @@ package java_Coding_Interview_Questions;
 public class  Q2_ReverseNegativeNumber {
     public static void main(String[] args) {
 
-        System.out.println(reversedMethod(-35));
+        System.out.println(reversedMethod(-43));
 
 
     }
-    public static int reversedMethod(int number){
+    // Method declaration for reversedMethod
+// This method takes an integer as input and returns the reversed version of the negative number
 
-        String sayiString = String.valueOf(number);
-        String reversed = "-";
+public static int reversedMethod(int number){
 
-        for (int i = sayiString.length()-1; i >0 ; i--) {
-            reversed += sayiString.charAt(i);
+    // Convert the input number to a string
+    String stringNumber = String.valueOf(number);
 
-        }
+    // Initialize a new string variable with a value of "-"
+    // This variable will be used to store the reversed version of the input number
+    String reversed = "-";
 
-        int num = Integer.parseInt(reversed);
-        return num;
+    // Loop through each character of the stringNumber in reverse order
+    // Start from the last character (index stringNumber.length()-1) and end at the second character (index 1)
+    for (int i = stringNumber.length()-1; i > 0 ; i--) {
+        // Append each character to the reversed string
+        reversed += stringNumber.charAt(i);
+
     }
+
+    // Convert the reversed string back to an integer
+    int num = Integer.parseInt(reversed);
+
+    // Return the reversed integer
+    return num;
+}
 
 }
